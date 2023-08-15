c1f69cdaa99249daac0a9544fe6797a5c9feadc8
Thejvanth <thejvanth08@gmail.com>
2023-08-15 12:06:59 +0530

    Code Mobile First CSS

--- script.js
+++ script.js
@@ -2,14 +2,33 @@ const navMenu = document.getElementById("nav-menu");
 const navToggle = document.getElementById("nav-toggle");
 const navClose = document.getElementById("nav-close");
 
-navMenu.addEventListener("click", () => {
-  navMenu.classList.add("show-menu");
-});
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // when window is scrolled vertically i.e greater than 50,
+  // add the scroll class to the header tag
+  // console.log(this); this refers window object which is calling the function as a cb
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
 
 navToggle.addEventListener("click", () => {
-  navMenu.classList.add("show-menu");
+  navMenu.classList.toggle("show-menu");
 });
 
 navClose.addEventListener("click", () => {
-  navClose.classList.remove("show-menu");
+  console.log("close");
+  navMenu.classList.toggle("show-menu");
 });
+
+window.addEventListener("scroll", scrollHeader);
+
+
+// Show scroll up (when to show it)
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350 ? scrollUp.classList.add("show-scroll")
+                      : scrollUp.classList.remove("show-scroll");
+}
+
+window.addEventListener("scroll", scrollUp);
